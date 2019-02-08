af71d1c3ea2f373dcbc16891250d6327dcadf1cf
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 22:10:47 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+ i=0 
+ new_array = []
+ while i < collection.index
+    if yield collection[i] == true
+    new_string = yield collection[i]
+    new_array << new_string
+    end
+ end
+return new_array
 end
