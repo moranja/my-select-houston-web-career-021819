fd4235ca5ec20634f7f78541ef81ed88f74bd464
Adam Moran <jeffadammoran@gmail.com>
2019-02-08 22:12:38 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,10 @@ def my_select(collection)
  i=0 
  new_array = []
  while i < collection.length
-    if yield collection[i] == true
-    new_string = yield collection[i]
-    new_array << new_string
-    end
+    #if yield collection[i] == true
+#new_string = yield collection[i]
+    #new_array << new_string
+    #end
     i+=1
  end
 return new_array
